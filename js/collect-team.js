770c0b7ca3285c4ed1dc322fd31b5c9de22c3227
Aliaksei Karzhou <alexius90e@gmail.com>
2025-03-03 23:24:03 +0300

    feat: implemented adding analitics

--- js/collect-team.js
+++ js/collect-team.js
@@ -1,20 +1,23 @@
-const employeesLimit = 4;
+const employeesLimit = 3;
 
 const collectTeamObj = {
+  manager: null,
+  analytic: null,
+  employees: [],
   blockEl: document.querySelector('.collect-team'),
   orderEl: document.querySelector('.collect-team__team-order'),
   orderFormEl: document.querySelector('.collect-team__team-order-form'),
   orderFormTeamInputEl: document.querySelector('.collect-team__team-order-form-ids'),
   collectBtnEl: document.querySelector('.collect-team__collect-button'),
-  stepManagerEl: document.querySelector('.collect-team__step_manager'),
-  stepWorkerEls: document.querySelectorAll('.collect-team__step_worker'),
+  stepsWrapperEl: document.querySelector('.collect-team__steps'),
+  stepEls: document.querySelectorAll('.collect-team__step'),
   teamEmployeesEl: document.querySelector('.collect-team__team-employees'),
   managersEl: document.querySelector('.collect-team__managers'),
+  analyticsEl: document.querySelector('.collect-team__analytics'),
   employeesEl: document.querySelector('.collect-team__employees'),
   managerCardEls: document.querySelectorAll('.collect-team__managers .collect-team__card'),
+  analyticCardEls: document.querySelectorAll('.collect-team__analytics .collect-team__card'),
   employeCardEls: document.querySelectorAll('.collect-team__employees .collect-team__card'),
-  manager: null,
-  employees: [],
   totalEl: document.querySelector('.collect-team__team-employe_total'),
   totalInfo: document.querySelector('.collect-team__team-employe-details-block-info'),
   totalResetEl: document.querySelector('.collect-team__team-employe-delete-button_reset'),
@@ -39,6 +42,16 @@ collectTeamObj.managerCardEls.forEach((managerCardEl) => {
     if (isAddButton) {
       const manager = getCollectTeamCardData(event.currentTarget);
       addManager(manager);
+    }
+  });
+});
+
+collectTeamObj.analyticCardEls.forEach((analyticCardEl) => {
+  analyticCardEl.addEventListener('click', (event) => {
+    const isAddButton = event.target.classList.contains('collect-team__card-add-button');
+    if (isAddButton) {
+      const analytic = getCollectTeamCardData(event.currentTarget);
+      addAnalytic(analytic);
     }
   });
 });
@@ -83,9 +96,6 @@ function getCollectTeamCardData(employeCardEl) {
     name: employeCardEl.querySelector('.collect-team__card-info-name'),
     role: employeCardEl.querySelector('.collect-team__card-info-role'),
     image: employeCardEl.querySelector('.collect-team__card-photo-image'),
-    speed: employeCardEl.querySelector('.collect-team__card-info-details-speed'),
-    experience: employeCardEl.querySelector('.collect-team__card-info-details-experience'),
-    price: employeCardEl.querySelector('.collect-team__card-info-details-price'),
   };
 
   const data = {
@@ -93,9 +103,6 @@ function getCollectTeamCardData(employeCardEl) {
     name: elements.name ? elements.name.dataset.name : '',
     role: elements.role ? elements.role.dataset.role : '',
     imageSrc: elements.image ? elements.image.dataset.image : '',
-    speed: elements.speed ? elements.speed.dataset.rating : '',
-    experience: elements.experience ? elements.experience.dataset.rating : '',
-    price: elements.price ? elements.price.dataset.rating : '',
   };
 
   return data;
@@ -108,9 +115,6 @@ function getEmployePageData(employeCardEl) {
     card: employeCardEl,
     name: employeCardEl.querySelector('.employe__title'),
     image: employeCardEl.querySelector('.employe__photo-image'),
-    speed: employeCardEl.querySelector('.employe__details-speed'),
-    experience: employeCardEl.querySelector('.employe__details-experience'),
-    price: employeCardEl.querySelector('.employe__details-price'),
   };
 
   const data = {
@@ -118,44 +122,75 @@ function getEmployePageData(employeCardEl) {
     name: elements.name ? elements.name.dataset.name : '',
     role: elements.card ? elements.card.dataset.role : '',
     imageSrc: elements.image ? elements.image.dataset.image : '',
-    speed: elements.speed ? elements.speed.dataset.rating : '',
-    experience: elements.experience ? elements.experience.dataset.rating : '',
-    price: elements.price ? elements.price.dataset.rating : '',
   };
 
   return data;
 }
 
 function updateCollectTeam() {
-  if (collectTeamObj.managersEl && collectTeamObj.employeesEl) {
+  if (collectTeamObj.managersEl && collectTeamObj.employeesEl && collectTeamObj.analyticsEl) {
     if (collectTeamObj.manager === null) {
       collectTeamObj.managersEl.classList.add('active');
+      collectTeamObj.analyticsEl.classList.remove('active');
       collectTeamObj.employeesEl.classList.remove('active');
-      if (collectTeamObj.stepManagerEl) collectTeamObj.stepManagerEl.classList.remove('active');
+    } else if (collectTeamObj.manager !== null && collectTeamObj.analytic === null) {
+      collectTeamObj.managersEl.classList.remove('active');
+      collectTeamObj.analyticsEl.classList.add('active');
+      collectTeamObj.employeesEl.classList.remove('active');
+    } else if (
+      collectTeamObj.manager !== null &&
+      collectTeamObj.analytic !== null &&
+      collectTeamObj.employees.length === employeesLimit
+    ) {
+      collectTeamObj.managersEl.classList.remove('active');
+      collectTeamObj.analyticsEl.classList.remove('active');
+      collectTeamObj.employeesEl.classList.remove('active');
     } else {
       collectTeamObj.managersEl.classList.remove('active');
+      collectTeamObj.analyticsEl.classList.remove('active');
       collectTeamObj.employeesEl.classList.add('active');
-      if (collectTeamObj.stepManagerEl) collectTeamObj.stepManagerEl.classList.add('active');
     }
   }
 
-  [...collectTeamObj.stepWorkerEls].forEach((stepEl, index) => {
-    if (collectTeamObj.manager === null) {
-      stepEl.classList.remove('active');
-      hideElement(stepEl);
-    } else if (index > collectTeamObj.employees.length) {
-      stepEl.classList.remove('active');
-      hideElement(stepEl);
-    } else if (index === collectTeamObj.employees.length) {
-      stepEl.classList.remove('active');
-      showElement(stepEl);
-    } else {
-      stepEl.classList.add('active');
-      showElement(stepEl);
+  [...collectTeamObj.stepEls].forEach((stepEl, index) => {
+    if (collectTeamObj.manager === null && collectTeamObj.manager === null) {
+      if (index <= 0) {
+        stepEl.classList.remove('active');
+      } else {
+        hideElement(stepEl);
+      }
+    } else if (collectTeamObj.manager !== null && collectTeamObj.analytic === null) {
+      if (index === 0) {
+        stepEl.classList.add('active');
+        showElement(stepEl);
+      } else if (index === 1) {
+        stepEl.classList.remove('active');
+        showElement(stepEl);
+      } else {
+        stepEl.classList.remove('active');
+        hideElement(stepEl);
+      }
+    } else if (collectTeamObj.manager !== null && collectTeamObj.analytic !== null) {
+      const activeTeamMateIndex = collectTeamObj.employees.length + 2;
+
+      if (index < activeTeamMateIndex) {
+        stepEl.classList.add('active');
+        showElement(stepEl);
+      } else if (index === activeTeamMateIndex) {
+        stepEl.classList.remove('active');
+        showElement(stepEl);
+      } else {
+        stepEl.classList.remove('active');
+        hideElement(stepEl);
+      }
     }
   });
 
-  if (collectTeamObj.manager === null && collectTeamObj.employees.length === 0) {
+  if (
+    collectTeamObj.manager === null &&
+    collectTeamObj.analytic === null &&
+    collectTeamObj.employees.length === 0
+  ) {
     hideElement(collectTeamObj.totalEl);
   } else {
     showElement(collectTeamObj.totalEl);
@@ -175,22 +210,23 @@ function updateTotalInfo(number) {
 function updateTeamCards() {
   updateLocalStorage();
 
-  const { manager, employees, totalInfo, orderFormTeamInputEl } = collectTeamObj;
+  const { manager, analytic, employees, totalInfo, orderFormTeamInputEl } = collectTeamObj;
 
-  const teamData = manager === null ? employees : [manager, ...employees];
+  const teamData = [manager, analytic, ...employees].filter((item) => item !== null);
 
   if (orderFormTeamInputEl)
     orderFormTeamInputEl.value = JSON.stringify(teamData.map((item) => item.id));
 
   if (totalInfo) totalInfo.innerText = updateTotalInfo(teamData.length);
 
-  if (teamData.length >= employeesLimit + 1) {
+  if (teamData.length >= employeesLimit + 2) {
     collectTeamObj.orderEl.classList.add('active');
     hideElement(collectTeamObj.teamEmployeesEl);
+    hideElement(collectTeamObj.stepsWrapperEl);
   }
 
   const teamCards = [...teamData].map((teamItemData) => {
-    const { id, name, role, imageSrc, speed, experience, price } = teamItemData;
+    const { id, name, role, imageSrc } = teamItemData;
 
     const teamCard = document.createElement('div');
     teamCard.classList.add('collect-team__team-employe');
@@ -218,14 +254,17 @@ function updateTeamCards() {
         <button class="collect-team__team-employe-delete-button"></button>
       </div>
     `;
-    
+
     teamCard.addEventListener('click', (event) => {
       const isCloseButton = event.target.classList.contains(
         'collect-team__team-employe-delete-button'
       );
-      const isManager = collectTeamObj.manager == null ? false : id === collectTeamObj.manager.id;
+      const isManager = role === 'Менеджер';
+      const isAnalytic = role === 'Аналитик';
+      const isEmploye = role === 'Специалист';
       if (isCloseButton && isManager) deleteManager(teamItemData);
-      if (isCloseButton && !isManager) deleteEmploye(teamItemData);
+      if (isCloseButton && isAnalytic) deleteAnalytic(teamItemData);
+      if (isCloseButton && isEmploye) deleteEmploye(teamItemData);
     });
 
     return teamCard;
@@ -240,16 +279,24 @@ function updateTeamCards() {
 function addManager(managerData) {
   if (managerData === null) return;
   collectTeamObj.manager = managerData;
-  collectTeamObj.managersEl.classList.remove('active');
-  collectTeamObj.employeesEl.classList.add('active');
   updateCollectTeam();
 }
 
 function deleteManager(managerData) {
   if (managerData === null) return;
   collectTeamObj.manager = null;
-  collectTeamObj.managersEl.classList.add('active');
-  collectTeamObj.employeesEl.classList.remove('active');
+  updateCollectTeam();
+}
+
+function addAnalytic(analyticData) {
+  if (analyticData === null) return;
+  collectTeamObj.analytic = analyticData;
+  updateCollectTeam();
+}
+
+function deleteAnalytic(analyticData) {
+  if (analyticData === null) return;
+  collectTeamObj.analytic = null;
   updateCollectTeam();
 }
 
@@ -260,6 +307,7 @@ function addEmploye(employeData) {
   if (collectTeamObj.employees.length === employeesLimit) {
     collectTeamObj.orderEl.classList.add('active');
     showElement(collectTeamObj.teamEmployeesEl);
+    showElement(collectTeamObj.stepsWrapperEl);
     return;
   }
   const employeCardEl = document.querySelector(`#${employeData.id}`);
@@ -268,7 +316,6 @@ function addEmploye(employeData) {
     collectTeamObj.employees = [...collectTeamObj.employees, employeData];
   }
   updateCollectTeam();
-  console.log(collectTeamObj.employees.length);
 }
 
 function deleteEmploye(employeData) {
@@ -283,24 +330,29 @@ function deleteEmploye(employeData) {
 
 function resetCollectTeam() {
   collectTeamObj.manager = null;
+  collectTeamObj.analytic = null;
   collectTeamObj.employees = [];
   collectTeamObj.employeCardEls.forEach((cardEl) => cardEl.classList.remove('active'));
   collectTeamObj.orderEl.classList.remove('active');
   showElement(collectTeamObj.teamEmployeesEl);
+  showElement(collectTeamObj.stepsWrapperEl);
   updateLocalStorage();
   updateCollectTeam();
 }
 
 function updateLocalStorage() {
   window.localStorage.setItem('collectTeamManager', JSON.stringify(collectTeamObj.manager));
+  window.localStorage.setItem('collectTeamAnalytic', JSON.stringify(collectTeamObj.analytic));
   window.localStorage.setItem('collectTeamEmployees', JSON.stringify(collectTeamObj.employees));
 }
 
 function checkLocalStorage() {
   const manager = window.localStorage.getItem('collectTeamManager');
+  const analytic = window.localStorage.getItem('collectTeamAnalytic');
   const employees = window.localStorage.getItem('collectTeamEmployees');
 
   if (manager !== null) collectTeamObj.manager = JSON.parse(manager);
+  if (analytic !== null) collectTeamObj.analytic = JSON.parse(analytic);
   if (employees !== null) {
     collectTeamObj.employees = JSON.parse(employees);
     const activeEmployeIds = collectTeamObj.employees.map((employe) => employe.id);
